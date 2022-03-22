cf4585fa98e81266e171d9da4fdbd05f5b1d5c5b
Raymond Wang <98355194+rwang7839@users.noreply.github.com>
2022-03-22 13:54:46 -0400

    sdfsaf

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
+from kivy.graphics import *
 
 
 class MyGrid(Widget):
-    pass
+    def on_touch_down(self, touch):
+        print("down", touch)
+
+    def on_touch_move(self, touch):
+        print("move", touch)
+
+    def on_touch_up(self, touch):
+        print("up", touch)
+
+    # def draw_lines(self):
+    #     with self.canvas:
+    #         Color(1., 0, 0)
+    #         Rectangle(pos=(10, 10), size = (500, 500))
 
 
 class CheckersAIApp(App):
@@ -12,4 +26,4 @@ class CheckersAIApp(App):
 
 
 if __name__ == "__main__":
-    CheckersAIApp().run()
+    grid = CheckersAIApp().run()
